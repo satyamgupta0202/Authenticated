02fea58a604cce73ea79bf27f90d064bd2cb0ece
satyam <2019335@iiitdmj.ac.in>
2021-11-23 09:22:15 +0530

    Jwt authenticated

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -1,11 +1,13 @@
 const User = require("../models/User.js");
-
+const jwt = require("jsonwebtoken");
 const handleError = (err) => {
   // console.log(err.message, err.code);
   let error = { email: " ", password: " " };
 
   if (err.code == 11000) {
-    return "Email Already Exist";
+    error.email = "email id already exist";
+    error.password = " ";
+    return error;
   }
 
   if (err.message.includes("user validation failed")) {
@@ -16,6 +18,13 @@ const handleError = (err) => {
   }
   return error;
 };
+
+const age = 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, "iiit jabalpur", {
+    expiresIn: age,
+  });
+};
 module.exports.signup_get = (req, res) => {
   res.render("sign-up");
 };
@@ -24,14 +33,18 @@ module.exports.login_get = (req, res) => {
 };
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
+
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: age * 1000,
+    });
+    res.status(201).json({ user: user._id });
   } catch (err) {
     res.status(400).send(handleError(err));
   }
-  // User.create({ username, password });
-  // res.send("New Sign-up");
 };
 module.exports.login_post = (req, res) => {
   const { username, password } = req.body;
